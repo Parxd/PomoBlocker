2b12840e35bc0ca243841e6a464c8088e4433c06
Daniel Park <102764808+Parxd@users.noreply.github.com>
2022-06-28 15:53:08 -0700

    Added main.py + settings.py

---
+++ src/main.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+from tkinter import messagebox
+import customtkinter as ctk
+from PIL import ImageTk, Image
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+
+class App():
+    def __init__(self):
+        self.master = ctk.CTk()
+        self.master.title("PomoBlocker")
+        # self.master.geometry("800x600")
+        self.master.iconbitmap(
+            r"C:\Users\14253\Important\Interests\Coding Projects\PomoBlocker\pomodoro.ico"
+        )
+        self.entries = []
+        self.websites = []
+        self.darkbgimage = ImageTk.PhotoImage(Image.open(
+            "C:\\Users\\14253\\Important\\Interests\\Coding Projects\\PomoBlocker\\bg.jpg"
+        ))
+        self.darkbgimagelabel = tk.Label(
+            self.master,
+            image = self.darkbgimage
+        )
+        self.darkbgimagelabel.grid(
+            row = 0,
+            column = 0,
+            columnspan = 2,
+            rowspan = 3,
+            sticky = "NSEW"
+        )
+
+        self.master.grid_columnconfigure(0, weight = 1)
+        self.master.grid_rowconfigure(1, weight = 1)
+        self.topframe = ctk.CTkFrame(
+            self.master,
+            width = 150,
+            corner_radius = 8,
+        )
+        self.topframe.grid(
+            row = 0,
+            column = 0,
+            sticky = "EW",
+            padx = 20,
+            pady = 15
+        )
+        self.mainframe = ctk.CTkFrame(
+            self.master,
+            width = 200
+        )
+        self.mainframe.grid(
+            row = 1,
+            column = 0,
+            sticky = "NESW",
+            padx = 20,
+            pady = (0, 16)
+        )
+
+        self.topframe.grid_rowconfigure(0, minsize=10)
+        self.topframe.grid_rowconfigure(5, weight=1)
+        self.topframe.grid_rowconfigure(8, minsize=20)
+        self.topframe.grid_rowconfigure(11, minsize=10)
+
+        self.title_label = ctk.CTkLabel(
+            self.topframe,
+            text = "PomoBlocker",
+            text_font = ("Roboto Medium", 18),
+            text_color = "#FF775B"
+        )
+        self.title_label.grid(
+            row = 1,
+            column = 0,
+            padx = 20,
+            pady = (12, 2)
+        )
+
+
+def main():
+    app = App()
+    app.master.mainloop()
+
+
+if __name__ == '__main__':
+    main()
