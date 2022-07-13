8ea7d33937a903623791d3365e1d11f12477de24
Daniel Park <102764808+Parxd@users.noreply.github.com>
2022-07-12 22:50:50 -0700

    File rename + testing different UI
    - Renamed countdown_timer.py to basic_pomotimer.py
        - basic_pomotimer.py will contain
        bare-bones/basic pomodoro timer
        functions only
    - Testing different UI design for the main app (buttons on top vs. buttons on left side to keep consistency with settings UI design)

--- src/main.py
+++ src/main.py
@@ -1,38 +1,33 @@
+from pathlib import Path
 import tkinter as tk
 from tkinter import messagebox
 import customtkinter as ctk
 from PIL import ImageTk, Image
 
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
 
 
 class App():
+    start = False
+    work = 1500
+    shortbreak = 300
+    longbreak = 900
+    workstring = f"{work // 60:02d} : {work % 60:02d}"
+    shortbreakstring = f"{shortbreak // 60:02d} : {shortbreak % 60:02d}"
+    longbreakstring = f"{longbreak // 60:02d} : {longbreak % 60:02d}"
+    pomocount = 0
+    cycle_length = 7
+
     def __init__(self):
         self.master = ctk.CTk()
+        self.master.geometry("800x600")
         self.master.title("PomoBlocker")
         self.master.iconbitmap(
-            "..\\res\\media\\pomodoro.ico"
+            Path(__file__).parent / "..\\res\\media\\pomodoro.ico"
         )
-        self.entries = []
-        self.websites = []
-        self.darkbgimage = ImageTk.PhotoImage(Image.open(
-            "..\\res\\media\\bg.jpg"
-        ))
-        self.darkbgimagelabel = tk.Label(
-            self.master,
-            image = self.darkbgimage
-        )
-        self.darkbgimagelabel.grid(
-            row = 0,
-            column = 0,
-            columnspan = 2,
-            rowspan = 3,
-            sticky = "NSEW"
-        )
-
         self.master.grid_columnconfigure(0, weight = 1)
         self.master.grid_rowconfigure(1, weight = 1)
+
         self.topframe = ctk.CTkFrame(
             self.master,
             width = 150,
@@ -45,35 +40,99 @@ class App():
             padx = 20,
             pady = 15
         )
-        self.mainframe = ctk.CTkFrame(
-            self.master,
-            width = 200
+        self.topframe.grid_rowconfigure((0, 1), weight = 1)
+        self.topframe.grid_columnconfigure(0, weight = 1)
+        self.title_label = ctk.CTkLabel(
+            self.topframe,
+            text = "PomoBlocker",
+            text_font = ("Roboto Medium", 18),
+            text_color = "#F48269"
         )
-        self.mainframe.grid(
+        self.title_label.grid(
+            row = 0,
+            column = 0,
+            padx = 20,
+            pady = (20, 5)
+        )
+
+        self.buttonsframe = ctk.CTkFrame(
+            self.topframe,
+            height = 100,
+            width = 800
+        )
+        self.buttonsframe.grid(
+            row = 1,
+            column = 0,
+            padx = 20,
+            pady = 20,
+            sticky = "NESW"
+        )
+        self.buttonsframe.grid_rowconfigure(0, weight = 1)
+        self.buttonsframe.grid_columnconfigure((0, 1, 2), weight = 1)
+
+        self.showworkframe = ctk.CTkButton(
+            self.buttonsframe,
+            text = "Work",
+            width = 200,
+            height = 35,
+            fg_color = "#FF775B",
+            text_font = ("Roboto Medium", 10)
+        )
+        self.showworkframe.grid(
+            row = 0,
+            column = 0,
+            padx = 20
+        )
+        self.showshortbreakframe = ctk.CTkButton(
+            self.buttonsframe,
+            text = "Short Break",
+            width = 200,
+            height = 35,
+            fg_color = "#FF775B",
+            text_font = ("Roboto Medium", 10)
+        )
+        self.showshortbreakframe.grid(
+            row = 0,
+            column = 1,
+            padx = 20,
+            pady = 20
+        )
+        self.showlongbreakframe = ctk.CTkButton(
+            self.buttonsframe,
+            text = "Long Break",
+            width = 200,
+            height = 35,
+            fg_color = "#FF775B",
+            text_font = ("Roboto Medium", 10)
+        )
+        self.showlongbreakframe.grid(
+            row = 0,
+            column = 2,
+            padx = 20
+        )
+
+        self.workframe = ctk.CTkFrame(
+            self.master
+        )
+        self.workframe.grid(
             row = 1,
             column = 0,
             sticky = "NESW",
             padx = 20,
             pady = (0, 16)
         )
-
-        self.topframe.grid_rowconfigure(0, minsize=10)
-        self.topframe.grid_rowconfigure(5, weight=1)
-        self.topframe.grid_rowconfigure(8, minsize=20)
-        self.topframe.grid_rowconfigure(11, minsize=10)
-
-        self.title_label = ctk.CTkLabel(
-            self.topframe,
-            text = "PomoBlocker",
-            text_font = ("Roboto Medium", 18),
-            text_color = "#FF775B"
+        self.shortbreakframe = ctk.CTkFrame(
+            self.master
         )
-        self.title_label.grid(
-            row = 1,
-            column = 0,
-            padx = 20,
-            pady = (12, 2)
+        self.longbreakframe = ctk.CTkFrame(
+            self.master
         )
+        self.workframe.grid_rowconfigure((0, 1), weight = 1)
+        self.workframe.grid_columnconfigure((0, 1, 2), weight = 1)
+        self.shortbreakframe.grid_rowconfigure((0, 1), weight = 1)
+        self.shortbreakframe.grid_columnconfigure((0, 1, 2), weight = 1)
+        self.longbreakframe.grid_rowconfigure((0, 1), weight = 1)
+        self.longbreakframe.grid_columnconfigure((0, 1, 2), weight = 1)
 
 
 def main():
